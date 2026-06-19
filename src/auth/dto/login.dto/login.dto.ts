0c2addce7d13e754b43ecff043020cc15c80e368
Duy <duylaptrinh03@gmail.com>
2026-06-19 12:07:24 +0700

    feat: create LoginDto for user authentication with validation decorators

--- src/auth/dto/login.dto/login.dto.ts
+++ src/auth/dto/login.dto/login.dto.ts
@@ -2,11 +2,17 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
 
 export class LoginDto {
-  @ApiProperty({ example: 'user@example.com', description: 'Email của người dùng' })
+  @ApiProperty({
+    example: 'user@example.com',
+    description: 'Email của người dùng',
+  })
   @IsEmail()
   email: string;
 
-  @ApiProperty({ example: 'password123', description: 'Mật khẩu' })
+  @ApiProperty({
+    example: 'password123',
+    description: 'Mật khẩu',
+  })
   @IsString()
   password: string;
 }
